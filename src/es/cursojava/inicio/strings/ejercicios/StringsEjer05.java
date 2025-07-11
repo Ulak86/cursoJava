fd2036c8b0d47fcb1c90d4a26c64e5a0acae9720
pvill <pvill@Ulak-Lenovo>
2025-07-11 03:25:01 +0200

    test

--- src/es/cursojava/inicio/strings/ejercicios/StringsEjer05.java
+++ src/es/cursojava/inicio/strings/ejercicios/StringsEjer05.java
@@ -83,7 +83,7 @@ public class StringsEjer05 {
 			dotPosition = mailSeparacion.indexOf('.');
 			mailSeparacion = mailSeparacion.substring(1, dotPosition);
 
-			if (mailSeparacion.length()!= 2) {
+			if (mailSeparacion.length() < 2) {
 
 				separacion = true;
 			}
@@ -110,23 +110,26 @@ public class StringsEjer05 {
 
 		if (espacios || arroba || punto || separacion || caracteres) {
 
+			System.out.println();
 			System.out.println("Your email address in invalid and containes the following errors");
+
 			if (espacios) {
-				System.out.println("Tiene espacios en blanco");
+				System.out.println("❌ Tiene espacios en blanco");
 			}
 			if (arroba) {
-				System.out.println("Tiene ninguna o mas de 1 arroba");
+				System.out.println("❌ Tiene ninguna o mas de 1 arroba");
 			}
 			if (punto) {
-				System.out.println("A la derecha de @ hay menos de 1 punto");
+				System.out.println("❌ A la derecha de @ hay menos de 1 punto");
 			}
 			if (separacion) {
-				System.out.println("Entre la arroba y el punto a su derecha no hay 2 caracteres");
+				System.out.println("❌ Entre la arroba y el punto a su derecha no hay 2 caracteres");
 			}
 			if (caracteres) {
-				System.out.println("Despues del ultimo punto no hay entre 2 o 6 caracteres");
+				System.out.println("❌ Despues del ultimo punto no hay entre 2 o 6 caracteres");
 			}
 		} else {
+			System.out.println();
 			System.out.println("Mail valido");
 		}
 	}
