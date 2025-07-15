f0a6f598279d3fe847afe4637911c55184f044d2
Ulak86_Eclipse_Getafe <Tardes@GET-11>
2025-07-15 17:47:36 +0200

    Arrays Ejercicio 3

--- src/es/cursojava/inicio/arrays/EjercicioArrays03.java
+++ src/es/cursojava/inicio/arrays/EjercicioArrays03.java
@@ -17,100 +17,114 @@ public class EjercicioArrays03 {
 
 	public static void main(String[] args) {
 
-		String[][] aulas = new String[2][3];
+//		String[][] aulas = new String[2][3];
+		String[][] aulas = { { null, "pedro", "diego" }, { "ana", null, "marta" } };
+		int opcion = 0;
 
-//		String[][] aulas = { { null, "pedro", "diego" }, { "ana", null, "marta" } };
+		do {
+			System.out.println("===========MENU===========");
+			System.out.println("Elige una opción");
+			System.out.println("1. Insertar alumno");
+			System.out.println("2. Mostrar todos los alumnos");
+			System.out.println("3. Buscar alumno");
+			System.out.println("4. Borrar alumno");
+			System.out.println("5. Salir");
 
-		System.out.println("1. Insertar alumno");
-		System.out.println("2. Mostrar alumno");
-		System.out.println("3. Buscar alumno");
-		System.out.println("4. Borrar alumno");
-		System.out.println("5. Salir");
+			Scanner scan = new Scanner(System.in);
+			opcion = scan.nextInt();
 
-		System.out.println("Elige una opción");
+			// switch nuevo
+			switch (opcion) {
+			case 1 -> {
 
-		Scanner scan = new Scanner(System.in);
-		int opcion = scan.nextInt();
+				System.out.println("===========OPCION 1=======");
+				int contadorCase1 = 0;
 
-		// switch nuevo
-		switch (opcion) {
-		case 1 -> {
-			for (int i = 0; i < aulas.length; i++) {
+				for (int i = 0; i < aulas.length; i++) {
 
-				for (int j = 0; j < aulas.length; j++) {
+					for (int j = 0; j < aulas[i].length; j++) {
 
-					System.out.println("Dame el nombre del alumno para el aula " + i + " y puesto " + j);
-					Scanner scant = new Scanner(System.in);
-					aulas[i][j] = scan.nextLine();
-
+						if (aulas[i][j] == null) {
+							System.out.println("Dame el nombre del alumno para el aula " + i + " y puesto " + j);
+							Scanner scann = new Scanner(System.in);
+							aulas[i][j] = scann.nextLine();
+							contadorCase1++;
+						}
+					}
 				}
+
+				if (contadorCase1 == 0) {
+					System.out.println("Todos los puestos de todas las aulas estan ocupados");
+				}
+
 			}
-		}
-		case 2 -> {
-			int puestosLibres = 0;
-			for (int i = 0; i < aulas.length; i++) {
+			case 2 -> {
+				System.out.println("===========OPCION 2=======");
+				int puestosLibres = 0;
+				for (int i = 0; i < aulas.length; i++) {
 
-				for (int j = 0; j < aulas.length; j++) {
+					for (int j = 0; j < aulas[i].length; j++) {
 
-					if (aulas[i][j] == null) {
-						puestosLibres++;
-						System.out.print("(vacio)" + "\t\t");
+						if (aulas[i][j] == null) {
+							puestosLibres++;
+							System.out.print("(vacio)" + "\t\t");
 
-					} else {
-						System.out.print(aulas[i][j] + "\t\t");
+						} else {
+							System.out.print(aulas[i][j] + "\t\t");
 
+						}
 					}
 					System.out.println();
 				}
-			}
-			if (puestosLibres == 0) {
-				System.out.println("Las aulas estan llenas");
-			}
-		}
-		case 3 -> {
-			System.out.println("A que alumno quieres buscar?");
-			Scanner scann = new Scanner(System.in);
-			String alumnoBuscado = scan.nextLine();
-			int alumnosCoincidentes = 0;
-
-			for (int i = 0; i < aulas.length; i++) {
-
-				for (int j = 0; j < aulas[i].length; j++) {
-
-					if (aulas[i][j].contains(alumnoBuscado)) {
-						System.out.print("El alumno buscado se encuentra en el aula " + i + "en el puesto" + j);
-						alumnosCoincidentes++;
-					}
-					if (alumnosCoincidentes == 0) {
-						System.out.println("No hay ningun alumno con ese nombre en este colegio");
-					}
-					System.out.println();
+				if (puestosLibres == 0) {
+					System.out.println("Las aulas estan llenas");
 				}
 			}
-		}
-		case 4 -> {
-			System.out.println("A que alumno quieres borrar?");
-			Scanner sc = new Scanner(System.in);
-			String alumnoABorrar = scan.nextLine();
-			int alumnosBorrables = 0;
-			for (int i = 0; i < aulas.length; i++) {
+			case 3 -> {
+				System.out.println("===========OPCION 3=======");
+				System.out.println("A que alumno quieres buscar?");
+				Scanner scanno = new Scanner(System.in);
+				String alumnoBuscado = scanno.nextLine();
+				int alumnosCoincidentes = 0;
 
-				for (int j = 0; j < aulas[i].length; j++) {
+				for (int i = 0; i < aulas.length; i++) {
 
-					if (aulas[i][j].contains(alumnoABorrar)) {
-						aulas[i][j] = null;
-						alumnosBorrables++;
+					for (int j = 0; j < aulas[i].length; j++) {
+
+						if (aulas[i][j]!= null && aulas[i][j].contains(alumnoBuscado)) {
+							System.out.println("El alumno buscado se encuentra en el aula " + i + " en el puesto " + j);
+							alumnosCoincidentes++;
+							}
+						}
+						System.out.println();
 					}
-					if (alumnosBorrables == 0) {
-						System.out.println("No hay ningun alumno con ese nombre en este colegio");
-					}
-					System.out.println();
+				if (alumnosCoincidentes == 0) {
+					System.out.println("No hay ningun alumno con ese nombre en este colegio");
 				}
 			}
-		}
-		case 5 -> System.out.println("Adios");
-		default -> System.out.println("Opción incorrecta");
-		}
+			case 4 -> {
+				System.out.println("===========OPCION 4=======");
+				System.out.println("A que alumno quieres borrar?");
+				Scanner sc = new Scanner(System.in);
+				String alumnoABorrar = sc.nextLine();
+				int alumnosBorrables = 0;
+				for (int i = 0; i < aulas.length; i++) {
 
-	}
-}
+					for (int j = 0; j < aulas[i].length; j++) {
+
+						if (aulas[i][j]!= null && aulas[i][j].contains(alumnoABorrar)) {
+							aulas[i][j] = null;
+							alumnosBorrables++;
+						}
+					}
+				}
+				if (alumnosBorrables == 0) {
+					System.out.println("No hay ningun alumno con ese nombre en este colegio");
+				}
+			}
+			case 5 -> System.out.println("Adios");
+			default -> System.out.println("Opción incorrecta");
+			}
+
+		}while(opcion!=5);
+}}
