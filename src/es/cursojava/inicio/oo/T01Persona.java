482b212ee9f4bc0f5278d98148aa0e72a2f409b3
Ulak86_Eclipse_Getafe <Tardes@GET-11>
2025-09-01 20:52:22 +0200

    POO init

---
+++ src/es/cursojava/inicio/oo/T01Persona.java
@@ -0,0 +1,14 @@
+package es.cursojava.inicio.oo;
+
+public class T01Persona {
+
+	String nombre;
+	String apellidos;
+	String dni;
+	int edad;
+	
+	public void correr () {
+		
+	}
+
+}
--- src/es/cursojava/inicio/utils/Utilidades.java
+++ src/es/cursojava/inicio/utils/Utilidades.java
@@ -1,9 +1,45 @@
 package es.cursojava.inicio.utils;
 
+import java.util.Scanner;
+
 public class Utilidades {
+	
 	public static int calculaEdad (int anoNacimiento) {
 		int edad = 2025 - anoNacimiento;
 		return edad;
+	}
+	
+	public static void pintaMenu(String[] menuStringArray) {
+
+		pintaMenu(menuStringArray, "Select an option");
+
+	}
+
+	public static void pintaMenu(String[] menuStringArray, String stringLinea) {
+
+		System.out.println(stringLinea);
+
+		for (String linea : menuStringArray) {
+			System.out.println(linea);
+		}
+
+	}
+
+	public static int pideDatoNumerico(String texto) {
+
+		System.out.println(texto);
+		Scanner sc = new Scanner(System.in);
+		int intIntroducido = sc.nextInt();
+		sc.nextLine();
+		return intIntroducido;
+	}
+
+	public static String pideDatoCadena(String texto) {
+
+		System.out.println(texto);
+		Scanner sc = new Scanner(System.in);
+		String strIntroducido = sc.nextLine();
+		return strIntroducido;
 	}
 
 }
