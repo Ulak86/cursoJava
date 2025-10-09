4820101243c04cb7aa10fc6c83d365fdd26876e9
Ulak_portable <47616011+Ulak86@users.noreply.github.com>
2025-10-09 22:55:20 +0200

    1

--- src/es/cursojava/inicio/colecciones/sets/ej02_super_mercado/Run.java
+++ src/es/cursojava/inicio/colecciones/sets/ej02_super_mercado/Run.java
@@ -1,17 +1,9 @@
 package es.cursojava.inicio.colecciones.sets.ej02_super_mercado;
 
-import java.awt.geom.GeneralPath;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
-import es.cursojava.inicio.colecciones.sets.ej02_super_mercado.model.Cliente;
-import es.cursojava.inicio.colecciones.sets.ej02_super_mercado.model.ProdAlimenticio;
 import es.cursojava.inicio.colecciones.sets.ej02_super_mercado.model.SuperMercado;
 import es.cursojava.inicio.colecciones.sets.ej02_super_mercado.util.CobrarCompra;
-import es.cursojava.inicio.colecciones.sets.ej02_super_mercado.util.Consola;
 import es.cursojava.inicio.colecciones.sets.ej02_super_mercado.util.DelEstanteAlCarro;
 import es.cursojava.inicio.colecciones.sets.ej02_super_mercado.util.IniciarSupermercado;
 import es.cursojava.inicio.colecciones.sets.ej02_super_mercado.util.Utilidades;
